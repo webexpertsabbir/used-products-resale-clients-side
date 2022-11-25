6350e184557d0cb58561579154c2515a0162d144
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-11-25 12:21:53 +0600

    footer

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Logo from '../../images/main logo.png';
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is footer</h2>
+            <footer className="footer p-10 text-base-content">
+                <div>
+                    <img src={Logo} alt="" />
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
         </div>
     );
 };
