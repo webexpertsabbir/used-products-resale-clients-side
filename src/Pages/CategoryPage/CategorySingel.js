2a4acb80a770bede0c3861e22fd6beb517b69774
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-11-28 16:53:57 +0600

    product page

--- src/Pages/CategoryPage/CategorySingel.js
+++ src/Pages/CategoryPage/CategorySingel.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Products from './Products';
 
 const CategorySingel = () => {
+    const { product } = useLoaderData();
+    // console.log(product)
+
     return (
-        <div>
-            
+        <div className='grid md:grid-cols-2'>
+            {
+                product.map((products, ids) => <Products
+                key={ids}
+                products={products}
+                ></Products>)
+            }
         </div>
     );
 };
